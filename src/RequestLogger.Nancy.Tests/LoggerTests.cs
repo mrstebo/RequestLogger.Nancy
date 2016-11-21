b8ffb59eb92ebf971f73f10dcd9c940a71533880
Steven Atkinson <mrstebouk@gmail.com>
2016-11-21 21:48:22 +0000

    Added tests to check errors.

--- src/RequestLogger.Nancy.Tests/LoggerTests.cs
+++ src/RequestLogger.Nancy.Tests/LoggerTests.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using System;
+using Moq;
 using Nancy;
 using Nancy.Testing;
 using NUnit.Framework;
@@ -23,13 +24,35 @@ namespace RequestLogger.Nancy.Tests
             var browser = new Browser(new ConfigurableBootstrapper(config =>
             {
                 config
-                    .Module<TestModule>()
+                    .Module<TestWithLoggerModule>()
                     .Dependency(_requestLogger.Object);
             }));
 
             browser.Get("/test", config => config.HttpRequest());
 
-            _requestLogger.Verify(x => x.Log(It.IsAny<RequestData>(), It.IsAny<ResponseData>()), Times.Once);
+            _requestLogger.Verify(x => x.Log(
+                    It.IsAny<RequestData>(),
+                    It.IsAny<ResponseData>()),
+                Times.Once);
+        }
+
+        [Test]
+        public void Enable_Should_LogError_For_Module()
+        {
+            var browser = new Browser(new ConfigurableBootstrapper(config =>
+            {
+                config
+                    .Module<TestWithLoggerModule>()
+                    .Dependency(_requestLogger.Object);
+            }));
+
+            Assert.Throws<Exception>(() => browser.Get("/test/error", config => config.HttpRequest()));
+
+            _requestLogger.Verify(x => x.LogError(
+                    It.IsAny<RequestData>(),
+                    It.IsAny<ResponseData>(),
+                    It.IsAny<Exception>()),
+                Times.Once);
         }
 
         [Test]
@@ -39,35 +62,63 @@ namespace RequestLogger.Nancy.Tests
             {
                 config.ApplicationStartup((container, pipelines) => Logger.Enable(pipelines, _requestLogger.Object));
                 config
-                    .Module<TestModule>();
+                    .Module<TestWithoutLoggerModule>();
             }));
 
             browser.Get("/test", config => config.HttpRequest());
 
-            _requestLogger.Verify(x => x.Log(It.IsAny<RequestData>(), It.IsAny<ResponseData>()), Times.Once);
+            _requestLogger.Verify(x => x.Log(
+                    It.IsAny<RequestData>(),
+                    It.IsAny<ResponseData>()),
+                Times.Once);
         }
 
-        private class TestModule : NancyModule
+        [Test]
+        public void Enable_Should_LogError_For_Application()
         {
-            public TestModule()
-                : base("/test")
+            var browser = new Browser(new ConfigurableBootstrapper(config =>
             {
-                Get["/"] = _ => "GET";
-                Post["/"] = _ => "POST";
-                Put["/"] = _ => "PUT";
-                Delete["/"] = _ => "DELETE";
-            }
+                config.ApplicationStartup((container, pipelines) => Logger.Enable(pipelines, _requestLogger.Object));
+                config
+                    .Module<TestWithoutLoggerModule>();
+            }));
 
-            public TestModule(IRequestLogger requestLogger)
+            Assert.Throws<Exception>(() => browser.Get("/test/error", config => config.HttpRequest()));
+
+            _requestLogger.Verify(x => x.LogError(
+                    It.IsAny<RequestData>(),
+                    It.IsAny<ResponseData>(),
+                    It.IsAny<Exception>()),
+                Times.Once);
+        }
+
+        private class TestWithLoggerModule : NancyModule
+        {
+            public TestWithLoggerModule(IRequestLogger requestLogger)
                 : base("/test")
             {
                 Logger.Enable(this, requestLogger);
 
                 Get["/"] = _ => "GET";
+                Get["/error"] = _ => { throw new Exception("ERROR"); };
                 Post["/"] = _ => "POST";
                 Put["/"] = _ => "PUT";
                 Delete["/"] = _ => "DELETE";
             }
+        }
+
+        private class TestWithoutLoggerModule : NancyModule
+        {
+            public TestWithoutLoggerModule()
+                : base("/test")
+            {
+                Get["/"] = _ => "GET";
+                Get["/error"] = _ => { throw new Exception("ERROR"); };
+                Post["/"] = _ => "POST";
+                Put["/"] = _ => "PUT";
+                Delete["/"] = _ => "DELETE";
+            }
+
         }
     }
 }
