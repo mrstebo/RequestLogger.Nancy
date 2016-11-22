6aff942cd501de81d59bde3d7f0647cf13d67bf5
Steven Atkinson <mrstebouk@gmail.com>
2016-11-22 20:07:07 +0000

    Added tests for POST, PUT and DELETE methods.

--- src/RequestLogger.Nancy.Tests/RequestLoggingTests.cs
+++ src/RequestLogger.Nancy.Tests/RequestLoggingTests.cs
@@ -169,6 +169,208 @@ namespace RequestLogger.Nancy.Tests
         }
 
         [Test]
+        public void Enable_Should_Log_POST_Request_For_Application()
+        {
+            var browser = new Browser(new ConfigurableBootstrapper(config =>
+            {
+                config.ApplicationStartup((container, pipelines) => RequestLogging.Enable(pipelines, _requestLogger.Object));
+                config
+                    .Module<TestModule>();
+            }));
+
+            browser.Post("/test", config =>
+            {
+                config.HostName("localhost");
+                config.HttpRequest();
+                config.Header("Accept", "text/html");
+                config.Body("REQUEST", "text/html");
+            });
+
+            _requestLogger.Verify(x => x.Log(
+                    It.Is<RequestData>(r =>
+                        r.Url == new Uri("http://localhost/test") &&
+                        r.HttpMethod == "POST" &&
+                        r.Header.ContainsKey("Accept") &&
+                        r.Header["Accept"].Any(y => y.Contains("text/html")) &&
+                        r.Content.SequenceEqual(Encoding.UTF8.GetBytes("REQUEST"))),
+                    It.Is<ResponseData>(r =>
+                        r.StatusCode == 200 &&
+                        r.ReasonPhrase == "OK" &&
+                        r.Header.ContainsKey("X-Test") &&
+                        r.Header["X-Test"][0] == "TEST" &&
+                        r.Content.SequenceEqual(Encoding.UTF8.GetBytes("TEST")))),
+                Times.Once);
+        }
+
+        [Test]
+        public void Enable_Should_Log_POST_Request_For_Module()
+        {
+            var browser = new Browser(new ConfigurableBootstrapper(config =>
+            {
+                config
+                    .Module<TestWithLoggerModule>()
+                    .Dependency(_requestLogger.Object);
+            }));
+
+            browser.Post("/test", config =>
+            {
+                config.HostName("localhost");
+                config.HttpRequest();
+                config.Header("Accept", "text/html");
+                config.Body("REQUEST", "text/html");
+            });
+
+            _requestLogger.Verify(x => x.Log(
+                    It.Is<RequestData>(r =>
+                        r.Url == new Uri("http://localhost/test") &&
+                        r.HttpMethod == "POST" &&
+                        r.Header.ContainsKey("Accept") &&
+                        r.Header["Accept"].Any(y => y.Contains("text/html")) &&
+                        r.Content.SequenceEqual(Encoding.UTF8.GetBytes("REQUEST"))),
+                    It.Is<ResponseData>(r =>
+                        r.StatusCode == 200 &&
+                        r.ReasonPhrase == "OK" &&
+                        r.Header.ContainsKey("X-Test") &&
+                        r.Header["X-Test"][0] == "TEST" &&
+                        r.Content.SequenceEqual(Encoding.UTF8.GetBytes("TEST")))),
+                Times.Once);
+        }
+
+        [Test]
+        public void Enable_Should_Log_PUT_Request_For_Application()
+        {
+            var browser = new Browser(new ConfigurableBootstrapper(config =>
+            {
+                config.ApplicationStartup((container, pipelines) => RequestLogging.Enable(pipelines, _requestLogger.Object));
+                config
+                    .Module<TestModule>();
+            }));
+
+            browser.Put("/test", config =>
+            {
+                config.HostName("localhost");
+                config.HttpRequest();
+                config.Header("Accept", "text/html");
+                config.Body("REQUEST", "text/html");
+            });
+
+            _requestLogger.Verify(x => x.Log(
+                    It.Is<RequestData>(r =>
+                        r.Url == new Uri("http://localhost/test") &&
+                        r.HttpMethod == "PUT" &&
+                        r.Header.ContainsKey("Accept") &&
+                        r.Header["Accept"].Any(y => y.Contains("text/html")) &&
+                        r.Content.SequenceEqual(Encoding.UTF8.GetBytes("REQUEST"))),
+                    It.Is<ResponseData>(r =>
+                        r.StatusCode == 200 &&
+                        r.ReasonPhrase == "OK" &&
+                        r.Header.ContainsKey("X-Test") &&
+                        r.Header["X-Test"][0] == "TEST" &&
+                        r.Content.SequenceEqual(Encoding.UTF8.GetBytes("TEST")))),
+                Times.Once);
+        }
+
+        [Test]
+        public void Enable_Should_Log_PUT_Request_For_Module()
+        {
+            var browser = new Browser(new ConfigurableBootstrapper(config =>
+            {
+                config
+                    .Module<TestWithLoggerModule>()
+                    .Dependency(_requestLogger.Object);
+            }));
+
+            browser.Put("/test", config =>
+            {
+                config.HostName("localhost");
+                config.HttpRequest();
+                config.Header("Accept", "text/html");
+                config.Body("REQUEST", "text/html");
+            });
+
+            _requestLogger.Verify(x => x.Log(
+                    It.Is<RequestData>(r =>
+                        r.Url == new Uri("http://localhost/test") &&
+                        r.HttpMethod == "PUT" &&
+                        r.Header.ContainsKey("Accept") &&
+                        r.Header["Accept"].Any(y => y.Contains("text/html")) &&
+                        r.Content.SequenceEqual(Encoding.UTF8.GetBytes("REQUEST"))),
+                    It.Is<ResponseData>(r =>
+                        r.StatusCode == 200 &&
+                        r.ReasonPhrase == "OK" &&
+                        r.Header.ContainsKey("X-Test") &&
+                        r.Header["X-Test"][0] == "TEST" &&
+                        r.Content.SequenceEqual(Encoding.UTF8.GetBytes("TEST")))),
+                Times.Once);
+        }
+
+        [Test]
+        public void Enable_Should_Log_DELETE_Request_For_Application()
+        {
+            var browser = new Browser(new ConfigurableBootstrapper(config =>
+            {
+                config.ApplicationStartup((container, pipelines) => RequestLogging.Enable(pipelines, _requestLogger.Object));
+                config
+                    .Module<TestModule>();
+            }));
+
+            browser.Delete("/test", config =>
+            {
+                config.HostName("localhost");
+                config.HttpRequest();
+                config.Header("Accept", "text/html");
+            });
+
+            _requestLogger.Verify(x => x.Log(
+                    It.Is<RequestData>(r =>
+                        r.Url == new Uri("http://localhost/test") &&
+                        r.HttpMethod == "DELETE" &&
+                        r.Header.ContainsKey("Accept") &&
+                        r.Header["Accept"].Any(y => y.Contains("text/html")) &&
+                        r.Content.Length == 0),
+                    It.Is<ResponseData>(r =>
+                        r.StatusCode == 200 &&
+                        r.ReasonPhrase == "OK" &&
+                        r.Header.ContainsKey("X-Test") &&
+                        r.Header["X-Test"][0] == "TEST" &&
+                        r.Content.SequenceEqual(Encoding.UTF8.GetBytes("TEST")))),
+                Times.Once);
+        }
+
+        [Test]
+        public void Enable_Should_Log_DELETE_Request_For_Module()
+        {
+            var browser = new Browser(new ConfigurableBootstrapper(config =>
+            {
+                config
+                    .Module<TestWithLoggerModule>()
+                    .Dependency(_requestLogger.Object);
+            }));
+
+            browser.Delete("/test", config =>
+            {
+                config.HostName("localhost");
+                config.HttpRequest();
+                config.Header("Accept", "text/html");
+            });
+
+            _requestLogger.Verify(x => x.Log(
+                    It.Is<RequestData>(r =>
+                        r.Url == new Uri("http://localhost/test") &&
+                        r.HttpMethod == "DELETE" &&
+                        r.Header.ContainsKey("Accept") &&
+                        r.Header["Accept"].Any(y => y.Contains("text/html")) &&
+                        r.Content.Length == 0),
+                    It.Is<ResponseData>(r =>
+                        r.StatusCode == 200 &&
+                        r.ReasonPhrase == "OK" &&
+                        r.Header.ContainsKey("X-Test") &&
+                        r.Header["X-Test"][0] == "TEST" &&
+                        r.Content.SequenceEqual(Encoding.UTF8.GetBytes("TEST")))),
+                Times.Once);
+        }
+
+        [Test]
         public void Enable_When_Pipelines_IsNull_ShouldThrow_ArgumentNullException()
         {
             Assert.Throws<ArgumentNullException>(() => RequestLogging.Enable((IPipelines) null, _requestLogger.Object));
